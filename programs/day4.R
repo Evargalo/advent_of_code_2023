3b1b341abd74527979b76dd8348b952e3636e90d
Olivier Pucher <olivier.pucher@insee.fr>
2023-12-04 18:05:00 +0100

    Update day4

--- programs/day4.R
+++ programs/day4.R
@@ -14,9 +14,7 @@ score_card <- function(s){
 }
 score_card(s)
 
-tibble(e) %>% 
-  rowwise %>% mutate(val=score_card(e)) %>% 
-  ungroup %>% summarise(sum(val))
+map_int(e,score_card) %>% sum
 
 # 28750
 
@@ -28,12 +26,9 @@ score_card <- function(s){
   l<-length(v[duplicated(v)])
   return (l)
 }
-tibble(e) %>% 
-  rowwise %>% mutate(val=score_card(e)) %>% 
-  mutate(n=1) %>% 
-  select(-e) %>% 
-  as.matrix -> mat
-mat
+
+mat <- matrix(ncol=2,byrow = FALSE,c(map_int(e,score_card),rep(1,length(e))))
+
 for(i in 1:(nrow(mat)-1)){
   k<-mat[i,1]
   l<-mat[i,2]
