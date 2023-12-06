ec9a4493d19a534b73447ca9d3af017076774baa
Olivier Pucher <olivier.pucher@insee.fr>
2023-12-06 16:47:34 +0100

    Update day6.R

--- programs/day6.R
+++ programs/day6.R
@@ -1,14 +1,13 @@
-
+# data ----
 e <- readLines(paste0(repAoC,"data/day6.txt"))
 
 times <- e[1] %>% str_split_1(" ") 
 times <- times[times!=""][-1] %>% as.numeric
-
 scores <- e[2] %>% str_split_1(" ") 
 scores <- scores[scores!=""][-1] %>% as.numeric
 
 
-# A----
+# Part A----
 calc_race <- function(time,press){
   if(press>=time) return(0)
   (time-press)*press
@@ -24,14 +23,14 @@ for(i in 1:4){
 res
 # 4811940
 
-# B ----
+# Part B ----
 t<-paste0(times,collapse = "") %>% as.numeric
 s<-paste0(scores,collapse = "") %>% as.numeric
 
-# p= press ; t= race duration ; s= current record
+# p= pressing time ; t= race duration ; s= current record
 # p² - t*p + s = 0
 delta=t^2-4*s
-p1 = (-t-sqrt(delta))/2
-p2 = (-t+sqrt(delta))/2
-p2-p1+1 %>% round
+p1 = (t-sqrt(delta))/2
+p2 = (t+sqrt(delta))/2
+floor(p2)-floor(p1)
 # 30077773
